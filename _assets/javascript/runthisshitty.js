6a95222605a1c474e29be808cda724a7d228042b
Chris van Wagoner <chrisv@fastmail.com>
2017-08-20 17:20:27 -0400

    Moved jquery to its own js file and continued cleaning

--- _assets/javascript/runthisshitty.js
+++ _assets/javascript/runthisshitty.js
@@ -4,3 +4,12 @@
 //   duration: 1000,
 //   easing: 'easeInOutElastic' 
 // });
+
+
+// smooth scrolling to in-page anchor tag
+jQuery(document).on('click', 'a[href*="#"]', function(event){
+	event.preventDefault();
+	jQuery('html, body').animate({
+		scrollTop: $( $.attr(this, 'href') ).offset().top
+ 	}, 500);
+});
