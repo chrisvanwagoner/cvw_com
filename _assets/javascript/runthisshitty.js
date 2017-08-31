4ff590dcd8822975e9208c6ab94027d7b406efed
Chris van Wagoner <chrisv@fastmail.com>
2017-08-31 11:16:44 -0400

    Cleaning up code and comments

--- _assets/javascript/runthisshitty.js
+++ _assets/javascript/runthisshitty.js
@@ -1,4 +1,4 @@
-// smooth scrolling to in-page anchor tag
+/* smooth scrolling to in-page anchor tag */
 jQuery(document).on('click', 'a[href*="#"]', function(event){
 	event.preventDefault();
 	jQuery('html, body').animate({
@@ -6,11 +6,11 @@ jQuery(document).on('click', 'a[href*="#"]', function(event){
  	}, 750);
 });
 
-// ==============================
-// Anime.js animations
-// ==============================
+/* ==============================
+Anime.js animations
+============================== */
 
-// Fade in/up the Header block
+/* Fade in/up the entire Header block */
 var headerFadeUp = anime({
   targets: '.header__content-wrap',
   translateY: 50,
@@ -21,6 +21,7 @@ var headerFadeUp = anime({
   opacity: 0,
 });
 
+/* Animation for Header Name */
 var headerNameFadeIn = anime({
 	targets: '.header__name',
 	translateX: 50,
@@ -30,6 +31,7 @@ var headerNameFadeIn = anime({
 	direction: 'reverse',
 });
 
+/* Animation for Header Text */
 var headerSummaryFadeIn = anime({
 	targets: '.header__summary',
 	translateY: 50,
@@ -39,8 +41,7 @@ var headerSummaryFadeIn = anime({
 	direction: 'reverse',
 });
 
-// Work section slides in from the left
-// animation object
+/* #Work animation object. Section slides in from the left */
 var slideWorkTitleFromLeft = {
   targets: '#work',
   translateX: 200,
@@ -49,22 +50,20 @@ var slideWorkTitleFromLeft = {
   opacity: 1,
 };
 
-// Prepare #work befor applying animation
+/* Prepare #work befor applying animation */
 var el = document.getElementById('work');
 el.style.left = "-200px";
 el.style.opacity = "0.5";
 
-// Animate #work when scrolled into viewport
+/* Animate #work when scrolled into viewport */
 $(window).scroll(function() {
 	var elementDistanceToWindowTop = $('#work').offset().top,
 		elementHeight = $('#work').outerHeight(),
 		viewportHeight = $(window).height(),
 		distanceFromTopOfSite = $(this).scrollTop();
   	var ninetyPercentOfViewportHeight = viewportHeight * 0.9;
-	// console.log("elementDistanceToWindowTop is " + elementDistanceToWindowTop);
-	// console.log("elementHeight is " + elementHeight);
-	// console.log("viewportHeight is " + viewportHeight);
-	// console.log("distanceFromTopOfSite is " + distanceFromTopOfSite);
+  	/* Print the variables' values to the console */
+	// console.log("elementDistanceToWindowTop is " + elementDistanceToWindowTop); console.log("elementHeight is " + elementHeight); console.log("viewportHeight is " + viewportHeight); console.log("distanceFromTopOfSite is " + distanceFromTopOfSite);
 	if (distanceFromTopOfSite > (elementDistanceToWindowTop + elementHeight - ninetyPercentOfViewportHeight) ) {
 		console.log('element is in the viewport.');
 		anime(slideWorkTitleFromLeft);
