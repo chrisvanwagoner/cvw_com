a1ea03b4bbec80070a3d38657e3a12136b19caa4
Chris van Wagoner <chrisvanwagoner@gmail.com>
2017-09-25 00:53:05 -0400

    Adding images

--- src/js/runthis.js
+++ src/js/runthis.js
@@ -1,4 +1,6 @@
-/* smooth scrolling to in-page anchor tag */
+/* runthis.js */
+
+/* Smooth scrolling to in-page anchor tag */
 jQuery(document).on('click', 'a[href*="#"]', function(event){
 	event.preventDefault();
 	jQuery('html, body').animate({
@@ -57,33 +59,4 @@ var headerSummaryFadeIn = anime({
 	easing: 'easeInOutQuad',
 	loop: 1,
 	direction: 'reverse',
-});
-
-/* #Work animation object. Section slides in from the left */
-// var slideWorkTitleFromLeft = {
-//   targets: '#work',
-//   translateX: 200,
-//   duration: 1000,
-//   easing: 'easeInOutQuad',
-//   opacity: 1,
-// };
-
-/* Prepare #work befor applying animation */
-// var el = document.getElementById('work');
-// el.style.left = "-200px";
-// el.style.opacity = "0.5";
-
-/* Animate #work when scrolled into viewport */
-$(window).scroll(function() {
-	var elementDistanceToWindowTop = $('#work').offset().top,
-		elementHeight = $('#work').outerHeight(),
-		viewportHeight = $(window).height(),
-		distanceFromTopOfSite = $(this).scrollTop();
-  	var ninetyPercentOfViewportHeight = viewportHeight * 0.9;
-  	/* Print the variables' values to the console */
-	// console.log("elementDistanceToWindowTop is " + elementDistanceToWindowTop); console.log("elementHeight is " + elementHeight); console.log("viewportHeight is " + viewportHeight); console.log("distanceFromTopOfSite is " + distanceFromTopOfSite);
-	if (distanceFromTopOfSite > (elementDistanceToWindowTop + elementHeight - ninetyPercentOfViewportHeight) ) {
-		console.log('element is in the viewport.');
-		anime(slideWorkTitleFromLeft);
-	}
 });
