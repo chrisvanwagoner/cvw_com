f7229e5eaee4e343abc5738c76f7474c550dda31
Chris van Wagoner <chrisvanwagoner@gmail.com>
2017-09-25 00:15:39 -0400

    Reshuffling by cloud storage :/

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# chrisvanwagoner.github.io
+# Personal site for Chris van Wagoner  
+
+Pulled together from half-finished/in-progress work from another git repo and put here.  
+This is a static HTML site, built with [Gulp](https://github.com/gulpjs/gulp) and [Sass](https://github.com/sass/sass). 
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  maps = require('gulp-sourcemaps'),
+  del = require('del'),
+  imagemin = require('gulp-imagemin');
+// var autoprefixer = require('gulp-autoprefixer');
+// var concat = require('gulp-concat');
+
+var browserSync = require('browser-sync').create();
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+/* Concatenate the listed scripts, map them, and output into a single file */
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'src/js/lib/anime.min.js',
+        'src/js/lib/wow.min.js',
+        'src/js/runthis.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('scripts.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('src/js'));
+});
+
+/* Take the file output by concatScripts, and minify it */
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("src/js/scripts.js")
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('src/js'));
+});
+
+/* Compile SCSS to CSS */
+gulp.task('sass', function() {
+  return gulp.src("src/scss/main.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('src/css'))
+      .pipe(browserSync.stream());
+});
+
+/* Look for changes to any SCSS file in scss directory, to any HTML file, and to any JS file in the js directory */
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('images', function(){
+  return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin([
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins: [{removeViewBox: true}]})
+  ]))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('del', function() {
+  del(['dist', 'src/css/main.css*']);
+});
+
+gulp.task("build", ['sass', 'minifyScripts', 'images'], function() {
+  return gulp.src([
+    "src/css/lib/normalize.css",
+    "src/css/lib/animate.min.css",
+    "src/css/main.css*",
+    "src/index.html",
+    "src/js/scripts.min.js",
+    "src/fonts/**",
+    "src/robots.txt"
+    ], { base: 'src'})
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["build"]);
