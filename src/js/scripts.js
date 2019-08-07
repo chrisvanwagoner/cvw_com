2459d8444c48f89ce5035696b017e3687db60afa
Chris van Wagoner <chrisv@fastmail.com>
2019-08-06 23:39:30 -0400

    build

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -174,7 +174,7 @@ Instanciate Wow.js object
 var wow = new WOW({
     boxClass:     'wow',      // animated element css class (default is wow)
     animateClass: 'animated', // animation css class (default is animated)
-    offset:       200,          // distance to the element when triggering the animation (default is 0)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
     mobile:       true,       // trigger animations on mobile devices (default is true)
     live:         true,       // act on asynchronously loaded content (default is true)
     callback:     function(box) {
