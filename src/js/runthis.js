73cc6747bb79a94f3a2d6f75b52b47966e129bd3
Chris van Wagoner <chrisv@fastmail.com>
2020-03-08 01:11:51 -0500

    Adding logic for client list and header scrolling

--- src/js/runthis.js
+++ src/js/runthis.js
@@ -16,3 +16,48 @@ var wow = new WOW({
     scrollContainer: null // optional scroll container selector, otherwise use window
   });
 wow.init();
+
+
+let clientEntry = document.querySelectorAll('.client');
+let fadeInLeft = document.querySelectorAll('.fadeInLeft');
+let windowHeight = window.innerHeight;
+let targetTop = windowHeight * 0.3;
+let targetBottom = windowHeight * 0.7;
+let checkHeight = (event) => {
+}
+
+// fadeInLeft.forEach((el) => {
+//   el.style.setProperty('opacity', 0);
+//   let rect = el.getBoundingClientRect();
+//   let top = rect.top;
+//   el.style.setProperty('transition', 'opacity 300ms ease-in-out');
+//   el.style.setProperty('opacity', 1);
+// });
+
+window.addEventListener('scroll', () => {
+  window.requestAnimationFrame(() => {
+
+    fadeInLeft.forEach((el) => {
+      let rect = el.getBoundingClientRect();
+      let top = rect.top;
+      el.style.setProperty('--left', ((top-el.offsetTop) / 2) + 'px');
+      el.style.setProperty('--opacity', (1 - (-top / 800)));
+    });
+
+    clientEntry.forEach((el) => {
+      let elRect = el.getBoundingClientRect();
+      let elName = el.querySelector('.client-name');
+      let distFromTop = elRect.top;
+      let weight;
+      if (distFromTop <= targetTop) {
+        weight = ((distFromTop / targetTop) * 300) + 100;
+        elName.style.setProperty('--wght', weight );
+      } else if ((distFromTop >= targetBottom) && (distFromTop <= windowHeight)) {
+        weight = (400 - (distFromTop - (targetBottom)) * 2);
+        elName.style.setProperty('--wght', weight );
+      } else {
+        elName.style.setProperty('--wght', 400 );
+      }
+    });
+  });
+});
