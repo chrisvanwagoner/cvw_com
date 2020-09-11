3afba1b97d1710a67802ce3f3b9b45468be5f397
Chris van Wagoner <chrisv@fastmail.com>
2020-09-11 11:46:56 -0500

    Add logic for mode switch button

--- src/js/runthis.js
+++ src/js/runthis.js
@@ -8,29 +8,32 @@ let targetTop = windowHeight * 0.4;
 let targetBottom = windowHeight * 0.7;
 let pathMain = document.querySelector('.main__svg path');
 let pathFooter = document.querySelector('.footer__svg path');
+let body = document.querySelector('body');
+let switchButton = document.querySelector('.mode-toggle');
+
 // paths.forEach((path, index) => {
-  // path.style.setProperty('--length', path.getTotalLength());
-  let pathMainAnimate = anime({
-    targets: pathMain,
-    loop: false,
-    direction: 'linear',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeOutExpo',
-    duration: 3000,
+// path.style.setProperty('--length', path.getTotalLength());
+let pathMainAnimate = anime({
+	targets: pathMain,
+	loop: false,
+	direction: 'linear',
+	strokeDashoffset: [anime.setDashoffset, 0],
+	easing: 'easeOutExpo',
+	duration: 3000,
 	autoplay: false,
 	delay: 500
-	
-  });
-  let pathFooterAnimate = anime({
-    targets: pathFooter,
-    loop: false,
-    direction: 'linear',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeOutExpo',
-    duration: 4000,
+
+});
+let pathFooterAnimate = anime({
+	targets: pathFooter,
+	loop: false,
+	direction: 'linear',
+	strokeDashoffset: [anime.setDashoffset, 0],
+	easing: 'easeOutExpo',
+	duration: 4000,
 	autoplay: false,
 	delay: 500
-  });
+});
 // });
 
 
@@ -43,79 +46,92 @@ let pathFooter = document.querySelector('.footer__svg path');
 // });
 
 if (window.innerWidth > 600) {
-  let events = () => {
-  
-    fadeLeft.forEach((el) => {
-      let top = el.getBoundingClientRect().top;
-      // let top = rect.top;
-      let speed = el.dataset.speed;
-      el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-      el.style.setProperty('--opacity', (1 - (-top / 800)));
-    });
-    fadeRight.forEach((el) => {
-      let top = el.getBoundingClientRect().top;
-      // let top = rect.top;
-      let speed = el.dataset.speed;
-      el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-      el.style.setProperty('--opacity', (1 - (-top / 800)));
-    });
+	let events = () => {
 
-    clientEntry.forEach((el) => {
-      let distFromTop = el.getBoundingClientRect().top;
-      let elName = el.querySelector('.client__name');
-      // let distFromTop = elRect.top;
-      let weight;
-      const maxWeight = 300;
-      if (distFromTop <= targetTop) {
-        weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
-        elName.style.setProperty('--wght', weight);
-        // } else if ((distFromTop >= targetBottom) && (distFromTop <= windowHeight)) {
-        //   weight = (maxWeight - (distFromTop - (targetBottom)) * 2);
-        //   elName.style.setProperty('--wght', weight );
-      } else {
-        elName.style.setProperty('--wght', maxWeight);
-      }
-    });
+		fadeLeft.forEach((el) => {
+			let top = el.getBoundingClientRect().top;
+			// let top = rect.top;
+			let speed = el.dataset.speed;
+			el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
+			el.style.setProperty('--opacity', (1 - (-top / 800)));
+		});
+		fadeRight.forEach((el) => {
+			let top = el.getBoundingClientRect().top;
+			// let top = rect.top;
+			let speed = el.dataset.speed;
+			el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
+			el.style.setProperty('--opacity', (1 - (-top / 800)));
+		});
 
-    // let h2Rect = document.querySelector('h2').getBoundingClientRect();
-    //   if ( h2Rect.top < targetTop ) {
-    //     line.play();
-    // }
-    // paths.forEach((path, line) => {
-      // let distFromTop = pathMain.getBoundingClientRect().top;
-      // if (distFromTop <= targetTop) { 
-      //   // console.log(path);
-      //   pathMainAnimate.play();
-      // }
-    // });
-    // line.play();
-  }
-  let checkScrollMain = () => {
-    let distFromTop = pathMain.getBoundingClientRect().top;
-    if (distFromTop <= targetBottom) { 
-      // console.log(path);
-      window.removeEventListener('scroll', checkScrollMain);
-      pathMainAnimate.play();
-    }
-  }
-  
-  let checkScrollFooter = () => {
-    let distFromTop = pathFooter.getBoundingClientRect().top;
-    if (distFromTop <= targetBottom) { 
-      // console.log(path);
-      window.removeEventListener('scroll', checkScrollFooter);
-      pathFooterAnimate.play();
-    }
-  }
+		clientEntry.forEach((el) => {
+			let distFromTop = el.getBoundingClientRect().top;
+			let elName = el.querySelector('.client__name');
+			// let distFromTop = elRect.top;
+			let weight;
+			const maxWeight = 300;
+			if (distFromTop <= targetTop) {
+				weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
+				elName.style.setProperty('--wght', weight);
+				// } else if ((distFromTop >= targetBottom) && (distFromTop <= windowHeight)) {
+				//   weight = (maxWeight - (distFromTop - (targetBottom)) * 2);
+				//   elName.style.setProperty('--wght', weight );
+			} else {
+				elName.style.setProperty('--wght', maxWeight);
+			}
+		});
 
-  window.addEventListener('scroll', checkScrollMain);
-  window.addEventListener('scroll', checkScrollFooter);
-  window.addEventListener('scroll', () => {
-    window.requestAnimationFrame(events);
-  });
-  window.addEventListener('resize', () => {
-    window.requestAnimationFrame(events);
-  });
+		// let h2Rect = document.querySelector('h2').getBoundingClientRect();
+		//   if ( h2Rect.top < targetTop ) {
+		//     line.play();
+		// }
+		// paths.forEach((path, line) => {
+		// let distFromTop = pathMain.getBoundingClientRect().top;
+		// if (distFromTop <= targetTop) { 
+		//   // console.log(path);
+		//   pathMainAnimate.play();
+		// }
+		// });
+		// line.play();
+		
+	}
+
+	let switchMode = (event) => {
+		if (body.classList.contains('dark-mode') == true) {
+			body.classList.remove('dark-mode');
+			event.target.innerHTML = 'Switch to dark mode';
+		} else {
+			body.classList.add('dark-mode');
+			event.target.innerHTML = 'Switch to light mode';
+		}
+	};
+
+	let checkScrollMain = () => {
+		let distFromTop = pathMain.getBoundingClientRect().top;
+		if (distFromTop <= targetBottom) {
+			// console.log(path);
+			window.removeEventListener('scroll', checkScrollMain);
+			pathMainAnimate.play();
+		}
+	}
+
+	let checkScrollFooter = () => {
+		let distFromTop = pathFooter.getBoundingClientRect().top;
+		if (distFromTop <= targetBottom) {
+			// console.log(path);
+			window.removeEventListener('scroll', checkScrollFooter);
+			pathFooterAnimate.play();
+		}
+	}
+
+	window.addEventListener('scroll', checkScrollMain);
+	window.addEventListener('scroll', checkScrollFooter);
+	window.addEventListener('scroll', () => {
+		window.requestAnimationFrame(events);
+	});
+	window.addEventListener('resize', () => {
+		window.requestAnimationFrame(events);
+	});
+	switchButton.addEventListener('click', switchMode);
 }
 
 // let preview = document.querySelector('#preview');
@@ -130,4 +146,3 @@ if (window.innerWidth > 600) {
 //   });
 //   name.addEventListener('mouseout', (e) => preview.style.setProperty('opacity', '0'));
 // });
-
