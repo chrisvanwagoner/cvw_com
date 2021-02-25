532b36ea244fc1ba9352926835f417600519a0eb
Chris van Wagoner <chrisv@fastmail.com>
2021-02-25 09:52:23 -0600

    gulp

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -229,9 +229,6 @@ let pathMain = document.querySelector('.main__svg path');
 let pathFooter = document.querySelector('.footer__svg path');
 let body = document.querySelector('body');
 let switchButton = document.querySelector('.mode-toggle');
-
-// paths.forEach((path, index) => {
-// path.style.setProperty('--length', path.getTotalLength());
 let pathMainAnimate = anime({
 	targets: pathMain,
 	loop: false,
@@ -253,109 +250,82 @@ let pathFooterAnimate = anime({
 	autoplay: false,
 	delay: 500
 });
-// });
 
 
-// fadeInLeft.forEach((el) => {
-//   el.style.setProperty('opacity', 0);
-//   let rect = el.getBoundingClientRect();
-//   let top = rect.top;
-//   el.style.setProperty('transition', 'opacity 300ms ease-in-out');
-//   el.style.setProperty('opacity', 1);
-// });
+let events = () => {
 
+	fadeLeft.forEach((el) => {
+		let top = el.getBoundingClientRect().top;
+		// let top = rect.top;
+		let speed = el.dataset.speed;
+		el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
+		el.style.setProperty('--opacity', (1 - (-top / 800)));
+	});
+	fadeRight.forEach((el) => {
+		let top = el.getBoundingClientRect().top;
+		// let top = rect.top;
+		let speed = el.dataset.speed;
+		el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
+		el.style.setProperty('--opacity', (1 - (-top / 800)));
+	});
 
-	let events = () => {
+	clientEntry.forEach((el) => {
+		let distFromTop = el.getBoundingClientRect().top;
+		let elName = el.querySelector('.client__name');
+		// let distFromTop = elRect.top;
+		let weight;
+		let skew;
+		let translateX;
+		const maxWeight = 300;
+		const maxSkew = 15;
+		const maxTranslate = 200;
+		if (distFromTop <= targetTop) {
+			weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
+			skew = ((targetTop - distFromTop) / targetTop) * maxSkew;
+			translateX = (((targetTop - distFromTop) / targetTop) ** 3) * maxTranslate;
+			
 
-		fadeLeft.forEach((el) => {
-			let top = el.getBoundingClientRect().top;
-			// let top = rect.top;
-			let speed = el.dataset.speed;
-			el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-			el.style.setProperty('--opacity', (1 - (-top / 800)));
-		});
-		fadeRight.forEach((el) => {
-			let top = el.getBoundingClientRect().top;
-			// let top = rect.top;
-			let speed = el.dataset.speed;
-			el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-			el.style.setProperty('--opacity', (1 - (-top / 800)));
-		});
-
-		clientEntry.forEach((el) => {
-			let distFromTop = el.getBoundingClientRect().top;
-			let elName = el.querySelector('.client__name');
-			// let distFromTop = elRect.top;
-			let weight;
-			let skew;
-			let translateX;
-			const maxWeight = 300;
-			const maxSkew = 15;
-			const maxTranslate = 200;
-			if (distFromTop <= targetTop) {
-				weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
-				skew = ((targetTop - distFromTop) / targetTop) * maxSkew;
-				translateX = (((targetTop - distFromTop) / targetTop) ** 3) * maxTranslate;
-				
-
-				elName.style.setProperty('--wght', weight);
-				if (window.innerWidth > 600) {
-					elName.style.setProperty('--skew', `${skew.toString()}deg`);
-					elName.style.setProperty('--translateX', `-${translateX.toString()}px`);
-				}
-			} else {
-				elName.style.setProperty('--wght', maxWeight);
-				elName.style.setProperty('--skew', '0');
-				elName.style.setProperty('--translateX', '0');
+			elName.style.setProperty('--wght', weight);
+			if (window.innerWidth > 600) {
+				elName.style.setProperty('--skew', `${skew.toString()}deg`);
+				elName.style.setProperty('--translateX', `-${translateX.toString()}px`);
 			}
-		});
-
-		// let h2Rect = document.querySelector('h2').getBoundingClientRect();
-		//   if ( h2Rect.top < targetTop ) {
-		//     line.play();
-		// }
-		// paths.forEach((path, line) => {
-		// let distFromTop = pathMain.getBoundingClientRect().top;
-		// if (distFromTop <= targetTop) { 
-		//   // console.log(path);
-		//   pathMainAnimate.play();
-		// }
-		// });
-		// line.play();
-		
-	}
-
-	// TODO: add check if system pref is set to light or dark mode
-	// TODO: change the button copy and class if light or dark
-
+		} else {
+			elName.style.setProperty('--wght', maxWeight);
+			elName.style.setProperty('--skew', '0');
+			elName.style.setProperty('--translateX', '0');
+		}
+	});
 	
+}
 
-	let checkScrollMain = () => {
-		let distFromTop = pathMain.getBoundingClientRect().top;
-		if (distFromTop <= targetBottom) {
-			// console.log(path);
-			window.removeEventListener('scroll', checkScrollMain);
-			pathMainAnimate.play();
-		}
+// TODO: add check if system pref is set to light or dark mode
+// TODO: change the button copy and class if light or dark
+
+let checkScrollMain = () => {
+	let distFromTop = pathMain.getBoundingClientRect().top;
+	if (distFromTop <= targetBottom) {
+		window.removeEventListener('scroll', checkScrollMain);
+		pathMainAnimate.play();
 	}
+}
 
-	let checkScrollFooter = () => {
-		let distFromTop = pathFooter.getBoundingClientRect().top;
-		if (distFromTop <= targetBottom) {
-			// console.log(path);
-			window.removeEventListener('scroll', checkScrollFooter);
-			pathFooterAnimate.play();
-		}
+let checkScrollFooter = () => {
+	let distFromTop = pathFooter.getBoundingClientRect().top;
+	if (distFromTop <= targetBottom) {
+		window.removeEventListener('scroll', checkScrollFooter);
+		pathFooterAnimate.play();
 	}
+}
 
-	window.addEventListener('scroll', checkScrollMain);
-	window.addEventListener('scroll', checkScrollFooter);
-	window.addEventListener('scroll', () => {
-		window.requestAnimationFrame(events);
-	});
-	window.addEventListener('resize', () => {
-		window.requestAnimationFrame(events);
-	});
+window.addEventListener('scroll', checkScrollMain);
+window.addEventListener('scroll', checkScrollFooter);
+window.addEventListener('scroll', () => {
+	window.requestAnimationFrame(events);
+});
+window.addEventListener('resize', () => {
+	window.requestAnimationFrame(events);
+});
 	
 
 let switchMode = (event) => {
