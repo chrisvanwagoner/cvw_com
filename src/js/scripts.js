9d89eb30abd128c3ab18f5e4d7e31ee28d238d95
Chris van Wagoner <chrisv@fastmail.com>
2021-03-02 10:40:22 -0600

    gulp

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -228,6 +228,7 @@ let targetBottom = windowHeight * 0.7;
 let pathMain = document.querySelector('.main__svg path');
 let pathFooter = document.querySelector('.footer__svg path');
 let body = document.querySelector('body');
+let preview = document.querySelector('#preview');
 let toggleButton = document.querySelector('.toggle');
 let pathMainAnimate = anime({
 	targets: pathMain,
@@ -251,12 +252,27 @@ let pathFooterAnimate = anime({
 	delay: 500
 });
 
+clientEntry.forEach((el) => {
+	let elName = el.querySelector('.client__name');
+	elName.addEventListener('mouseover', e => {
+		let elPreview = e.target.offsetParent.getAttribute('data-preview');
+		// console.log(elPreview);
+		preview.style.opacity = 1;
+		preview.style.backgroundImage = `url("../${elPreview}")`;
+		preview.style.setProperty('--x', `${e.pageX}px`);
+		preview.style.setProperty('--y', `${e.pageY}px`);
 
-let events = () => {
+	});
+	elName.addEventListener('mouseout', () => {
+		preview.style.opacity = 0;
+	});
+});
+
+
+const events = () => {
 
 	fadeLeft.forEach((el) => {
 		let top = el.getBoundingClientRect().top;
-		// let top = rect.top;
 		let speed = el.dataset.speed;
 		el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
 		el.style.setProperty('--opacity', (1 - (-top / 800)));
@@ -276,32 +292,25 @@ let events = () => {
 		let skew;
 		let translateX;
 		const maxWeight = 300;
-		const maxSkew = 15;
-		const maxTranslate = 200;
+		const maxSkew = 35;
+		const maxTranslate = 100;
 		if (distFromTop <= targetTop) {
 			weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
 			skew = ((targetTop - distFromTop) / targetTop) * maxSkew;
 			translateX = (((targetTop - distFromTop) / targetTop) ** 3) * maxTranslate;
-			
-
 			elName.style.setProperty('--wght', weight);
-			if (window.innerWidth > 600) {
-				elName.style.setProperty('--skew', `${skew.toString()}deg`);
-				elName.style.setProperty('--translateX', `-${translateX.toString()}px`);
-			}
+			elName.style.setProperty('--skew', `${skew.toString()}deg`);
+			elName.style.setProperty('--translateX', `-${translateX.toString()}px`);
 		} else {
 			elName.style.setProperty('--wght', maxWeight);
 			elName.style.setProperty('--skew', '0');
 			elName.style.setProperty('--translateX', '0');
 		}
 	});
-	
+
 }
 
-// TODO: add check if system pref is set to light or dark mode
-// TODO: change the button copy and class if light or dark
-
-let checkScrollMain = () => {
+const checkScrollMain = () => {
 	let distFromTop = pathMain.getBoundingClientRect().top;
 	if (distFromTop <= targetBottom) {
 		window.removeEventListener('scroll', checkScrollMain);
@@ -309,7 +318,7 @@ let checkScrollMain = () => {
 	}
 };
 
-let checkScrollFooter = () => {
+const checkScrollFooter = () => {
 	let distFromTop = pathFooter.getBoundingClientRect().top;
 	if (distFromTop <= targetBottom) {
 		window.removeEventListener('scroll', checkScrollFooter);
@@ -325,7 +334,7 @@ window.addEventListener('scroll', () => {
 window.addEventListener('resize', () => {
 	window.requestAnimationFrame(events);
 });
-	
+
 
 let toggleMode = (event) => {
 	if (body.classList.contains('dark-mode') == true) {
@@ -338,29 +347,17 @@ let toggleMode = (event) => {
 		event.target.innerHTML = '<span id="toggle-label" hidden>Switch to light mode</span>';
 	}
 };
-toggleButton.addEventListener('click', toggleMode);
 
+toggleButton.addEventListener('click', toggleMode);
 let colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
 const setColorScheme = e => {
-  if (e.matches) {
-    body.classList.add('dark-mode');
-  } else {
-    body.classList.add('light-mode');
-  }
+	if (e.matches) {
+		body.classList.add('dark-mode');
+	} else {
+		body.classList.add('light-mode');
+	}
 }
 
 setColorScheme(colorSchemeQueryList);
 colorSchemeQueryList.addEventListener(setColorScheme, null);
-
-// let preview = document.querySelector('#preview');
-// clientEntry.forEach((el) => {
-//   let name = el.querySelector('.client-name');
-//   let image = name.getAttribute('data-preview');
-//   name.addEventListener('mouseover', (e) => {
-//     window.setTimeout(() => {
-//       preview.style.setProperty('background-image', 'url(' + image + ')');
-//       preview.style.setProperty('opacity', '0.5');
-//     }, 250);
-//   });
-//   name.addEventListener('mouseout', (e) => preview.style.setProperty('opacity', '0'));
-// });
